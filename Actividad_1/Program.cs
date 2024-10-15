5bdfed3d90e45ad31b6e349a9b27b57ff8a85944
Alvarogogal <agorsua0610@g.educaand.es>
2024-10-15 21:24:34 +0200

    .

--- Actividad_1/Program.cs
+++ Actividad_1/Program.cs
@@ -1,32 +1,128 @@
 ﻿using System;
+using System.Collections;
 using Actividad_1.Entities;
-using Actividad_1.Items.Protections;
-using Actividad_1.Items.Weapons;
 
 namespace Actividad_1
 {
     internal class Program
     {
+        private static ArrayList characters = new ArrayList();
+
         public static void Main(string[] args)
         {
-            Character A1 = new Character("Juan",10.0,5.0,0.0);
-            Character A2 = new Character("Pepe", 10.0, 5.0, 0.0);
+            String userInput;
+            Console.WriteLine("Bienvenido a \"Ultimate battle simulator\"");
+
+            while (true)
+            {
+                Console.WriteLine("¿Cuantos jugadores van a jugar? (minimo de 2)");
+                userInput = Console.ReadLine();
+                try
+                {
+                    int n =int.Parse(userInput);
+                    if (n>1)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Deben haber un minimo de 2 jugadores");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("El valor debe ser un numero, inserte de nuevo el número de jugadores");
+                }
+            }
             
-            A1.addWeapon(new Axe());
-            A1.addProtection(new Shield());
-            A1.addPet(new Pet("Lukos",10,A1));
+            Console.WriteLine($"Perfecto, {userInput} jugadores, procedemos a crearlos"); 
             
-            A2.addWeapon(new Sword());
-            A2.addProtection(new Helmet());
-            A2.heal(5);
+            for (int i = 0; i < int.Parse(userInput); i++)
+            {
+                Console.WriteLine($"Procedemos con la creación del jugador {i}");
+                createPlayer();
+            }
 
-            Console.WriteLine(A1.seeInventory());
+            foreach (var c in characters)
+            {
+                char op;
+                Console.WriteLine($"Ahora vamos a equipar al personaje {c.name}, ¿Espada(e) o hacha(h)?");
+                op = Console.ReadLine().ToUpper()[0];
+                while (op!='E' || op!='H')
+                {
+                    Console.WriteLine("Esa opción no es valida, Espada(e) o hacha()h");
+                    op = Console.ReadLine().ToUpper()[0];
+                }
+            }
+            
+            //TODO: loop de batalla
 
-            // Console.WriteLine(A1.attack(A2));
-            // Console.WriteLine(A2.attack(A1));
-            // Console.WriteLine(A1.attack(A2));
-            // Console.WriteLine(A2.attack(A1));
+        }
 
+        
+        
+        private static void createPlayer()
+        {
+            char op;
+            String nombre;
+            double hp=0;
+            double dmg=0;
+            double armor=0;
+            
+            do
+            {
+                Console.WriteLine("Ingrese el nombre del jugador:");
+                nombre = Console.ReadLine();
+                Console.WriteLine($"{nombre}, ¿Es correcto? Y/N");
+                op = Console.ReadLine().ToUpper()[0];
+            } while (op != 'Y');
+
+            do
+            {
+                Console.WriteLine("Ingrese la salud del jugador:");
+                try
+                {
+                    hp = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{hp} de salud, ¿Es correcto? Y/N");
+                    op = Console.ReadLine().ToUpper()[0];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Escriba un número por favor\n");
+                    op = 'N';
+                }
+            } while (op!='Y');
+            
+            do
+            {
+                Console.WriteLine("Ingrese el daño del jugador:");
+                try
+                {
+                    dmg = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{dmg} de daño, ¿Es correcto? Y/N");
+                    op = Console.ReadLine().ToUpper()[0];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Escriba un número por favor\n");
+                    op = 'N';
+                }
+            } while (op!='Y');
+            
+            do
+            {
+                Console.WriteLine("Ingrese la armadura del jugador:");
+                try
+                {
+                    armor = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{armor} de armadura, ¿Es correcto? Y/N");
+                    op = Console.ReadLine().ToUpper()[0];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Escriba un número por favor\n");
+                    op = 'N';
+                }
+            } while (op!='Y');
+            
+            characters.Add(new Character(nombre,hp,dmg,armor));
         }
     }
 }
